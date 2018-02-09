cb61b6e99b455a2adf21e34b0d412283d2b4332d
Victor Zumpolle <victor@victorzumpolle.nl>
2018-02-09 10:16:35 +0100

    Update html: ol -> ul. Update js: Added consistency. Split up in more methods

--- opdracht 5 - spa/assets/js/app.js
+++ opdracht 5 - spa/assets/js/app.js
@@ -2,57 +2,60 @@
     "use strict";
 
     var app = {
-        init: function() {
-            routes.init();
-            nav.init();
-        }
-    };
-
-    var nav = {
         init: function () {
-            document.querySelectorAll("nav a").forEach(function (element){
-                element.addEventListener("click", function(event) {
-                    event.preventDefault();
-                    location.hash = this.hash;
-                });
+            routes.init()
+            this.handleEvents()
+        },
+        handleEvents: function () {
+            document.querySelectorAll("nav a").forEach(function (element) {
+                element.addEventListener("click", function (event) {
+                    event.preventDefault()
+                    location.hash = this.hash
+                })
             })
         }
-    };
+    }
 
     var routes = {
         current: "",
-        init: function() {
-            var self = this;
+        init: function () {
+            this.handleEvents()
+        },
+        handleEvents: function () {
+            var self = this
+
+            this.setHash()
 
             window.addEventListener("hashchange", function (event) {
-                self.current = location.hash;
-                sections.toggle(self.current);
-            });
-
+                self.current = location.hash
+                sections.toggle(self.current)
+            })
+        },
+        setHash: function () {
             if (location.hash) {
-                this.current = location.hash;
+                this.current = location.hash
             } else {
-                this.current = "#start";
+                this.current = "#start"
             }
 
-            sections.toggle(this.current);
-        }
-    };
+            sections.toggle(this.current)
+        }            
+    }
 
     var sections = {
-        sections: function() {
+        sections: function () {
             return document.querySelectorAll("section");
         },
         toggle: function (route) {
 
-            this.sections().forEach(function(element) {
-                element.classList.add("hidden");
-            });
+            this.sections().forEach(function (element) {
+                element.classList.add("hidden")
+            })
 
-            document.querySelector(route).classList.remove("hidden");
+            document.querySelector(route).classList.remove("hidden")
         }
-    };
+    }
 
-    app.init();
+    app.init()
 
-}());
+})()
