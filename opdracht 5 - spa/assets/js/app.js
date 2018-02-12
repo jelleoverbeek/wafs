663125b463c4e9ebc836cfe75769c3e2c7dcdf02
Jelle Overbeek <j.overbeek@recognize.nl>
2018-02-12 10:57:49 +0100

    removed semicolons

--- opdracht 5 - spa/assets/js/app.js
+++ opdracht 5 - spa/assets/js/app.js
@@ -1,63 +1,63 @@
 (function () {
-    "use strict";
+    "use strict"
 
     // General app object
     var app = {
         init: function() {
-            routes.init();
-            nav.init();
+            routes.init()
+            nav.init()
         }
-    };
+    }
 
     // Adding event listeners to a elements to prevent the default scroll behaviour
     var nav = {
         init: function () {
             document.querySelectorAll("nav a").forEach(function (element){
                 element.addEventListener("click", function(event) {
-                    event.preventDefault();
-                    location.hash = this.hash;
-                });
+                    event.preventDefault()
+                    location.hash = this.hash
+                })
             })
         }
-    };
+    }
 
     // Routes object that handles URL changes
     var routes = {
         current: "",
         init: function() {
-            var self = this;
+            var self = this
 
             window.addEventListener("hashchange", function (event) {
-                self.current = location.hash;
-                sections.toggle(self.current);
-            });
+                self.current = location.hash
+                sections.toggle(self.current)
+            })
 
             // Check if page already has a hash when loading and set the current to it.
             if (location.hash) {
-                this.current = location.hash;
+                this.current = location.hash
             } else {
-                this.current = "#start";
+                this.current = "#start"
             }
 
-            sections.toggle(this.current);
+            sections.toggle(this.current)
         }
-    };
+    }
 
     // Sections object with toggle function to show/hide sections
     var sections = {
         getSections: function() {
-            return document.querySelectorAll("section");
+            return document.querySelectorAll("section")
         },
         toggle: function (route) {
 
             this.getSections().forEach(function(element) {
-                element.classList.add("hidden");
-            });
+                element.classList.add("hidden")
+            })
 
-            document.querySelector(route).classList.remove("hidden");
+            document.querySelector(route).classList.remove("hidden")
         }
-    };
+    }
 
-    app.init();
+    app.init()
 
-}());
+}())
