e95c3e01a4b53fca250e30e1caa40c2061f5dad5
Jelle Overbeek <j.overbeek@recognize.nl>
2018-02-12 13:07:46 +0100

    added API call

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,7 @@
         init: function() {
             routes.init()
             nav.init()
+            api.getRecentTracks()
         }
     }
 
@@ -18,6 +19,49 @@
                     location.hash = this.hash
                 })
             })
+        }
+    }
+
+
+    var api = {
+        user: "jelleoverbeek",
+        key: "7ba5f2c9030c85c7b3f7c5816d87960c",
+        format: "json",
+        limit: 10,
+        getRecentTracks: function () {
+
+            var request = new XMLHttpRequest(),
+                url = "//ws.audioscrobbler.com/2.0?method=user.getrecenttracks&user=" + this.user + "&api_key=" + this.key + "&format=" + this.format + "&limit=" + this.limit
+
+            request.open('GET', url, true)
+
+            request.onload = function() {
+                if (request.status >= 200 && request.status < 400) {
+
+                    var data = JSON.parse(request.responseText),
+                        tracks = data.recenttracks.track,
+                        artist = data.recenttracks.track[0].artist["#text"],
+                        song = data.recenttracks.track[0].name
+
+                    document.querySelector("#the-track").innerHTML = artist + " - " + song
+
+                    console.log(data.recenttracks.track);
+
+                    tracks.forEach(function (track) {
+                        console.log(track);
+                        document.querySelector("#played-tracks").innerHTML += "<li>" + track.artist["#text"] + " - " + track.name + "</li>"
+                    })
+
+                } else {
+                    // We reached our target server, but it returned an error
+                }
+            }
+
+            request.onerror = function() {
+                console.log("error")
+            }
+
+            request.send()
         }
     }
 
