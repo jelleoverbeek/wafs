a5e6304b4668e538eeabc9d47a955d734cc90949
Jelle Overbeek <j.overbeek@recognize.nl>
2018-02-12 16:11:28 +0100

    added promise, added transparancy.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,15 +5,13 @@
     var app = {
         init: function() {
             nav.init()
-            api.getRecentTracks()
+            api.getData()
         }
     }
 
     // Adding event listeners to a elements to prevent the default scroll behaviour
     var nav = {
         init: function () {
-            console.log(document.querySelectorAll("nav a")[0]);
-
             document.querySelectorAll("nav a").forEach(function (element){
                 element.addEventListener("click", function(event) {
                     event.preventDefault()
@@ -23,51 +21,73 @@
         }
     }
 
+    var content = {
+        "current-track": "",
+        "track-list": [],
+        render: function () {
+            Transparency.render(document.getElementById('now-playing'), this);
+            Transparency.render(document.getElementById('recent-tracks'), this);
+        }
+    }
 
     var api = {
         user: "jelleoverbeek",
         key: "7ba5f2c9030c85c7b3f7c5816d87960c",
         format: "json",
-        limit: 10,
-        setCurrentTrack: function (track) {
-            document.querySelector("#the-track").innerHTML = track
-        },
-        setPlayedTracks: function (tracks) {
-            tracks.forEach(function (track) {
-                document.querySelector("#played-tracks-list").innerHTML += "<li>" + track.artist["#text"] + " - " + track.name + "</li>"
-            })
-        },
         getRecentTracks: function () {
-            var self = this,
-                request = new XMLHttpRequest(),
-                url = "//ws.audioscrobbler.com/2.0?method=user.getrecenttracks&user=" + this.user + "&api_key=" + this.key + "&format=" + this.format + "&limit=" + this.limit
+            var self = this;
 
-            request.open('GET', url, true)
+            return new Promise(function (resolve, reject) {
+                var request = new XMLHttpRequest(),
+                    limit = 10,
+                    url = "//ws.audioscrobbler.com/2.0?method=user.getrecenttracks&user=" + self.user + "&api_key=" + self.key + "&format=" + self.format + "&limit=" + limit
 
-            request.onload = function() {
-                if (request.status >= 200 && request.status < 400) {
+                request.open('GET', url, true)
 
-                    var data = JSON.parse(request.responseText),
-                        tracks = data.recenttracks.track,
-                        artist = data.recenttracks.track[0].artist["#text"],
-                        song = data.recenttracks.track[0].name
+                request.onload = function() {
+                    if (request.status >= 200 && request.status < 400) {
 
-                    self.setCurrentTrack(artist + " - " + song)
-                    self.setPlayedTracks(tracks)
+                        var data = JSON.parse(request.responseText),
+                            tracks = data.recenttracks.track,
+                            artist = data.recenttracks.track[0].artist["#text"],
+                            song = data.recenttracks.track[0].name
 
-                } else {
-                    // We reached our target server, but it returned an error
+                        content["current-track"] = artist + " - " + song
+
+                        tracks.forEach(function (track) {
+                            var artist = track.artist["#text"],
+                                song = track.name,
+                                trackName = artist + " - " + song
+
+                            content["track-list"].push({
+                                track: trackName
+                            })
+                        })
+
+                        resolve(data)
+
+                    } else {
+                        reject('error')
+                    }
                 }
-            }
 
-            request.onerror = function() {
-                console.log("error")
-            }
+                request.onerror = function() {
+                    reject('error');
+                }
 
-            request.send()
+                request.send()
+            });
+        },
+        getData: function () {
+            this.getRecentTracks()
+                .then(function () {
+                    content.render()
+                })
+                .catch(function (err) {
+                    console.error(err);
+                });
         }
     }
-
 
     // Sections object with toggle function to show/hide sections
     var sections = {
@@ -84,12 +104,12 @@
         }
     }
 
-    routie('current-track', function() {
-        sections.toggle("#current-track")
+    routie('now-playing', function() {
+        sections.toggle("#now-playing")
     });
 
-    routie('played-tracks', function() {
-        sections.toggle("#played-tracks")
+    routie('recent-tracks', function() {
+        sections.toggle("#recent-tracks")
     });
 
     app.init()
