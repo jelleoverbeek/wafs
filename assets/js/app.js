81f5cc944eb693b7d11deb1da0493d040cd9a51e
Jelle Overbeek <j.overbeek@recognize.nl>
2018-02-14 12:57:14 +0100

    added some styling, arrays are now filtered by img

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,10 +23,15 @@
 
     var content = {
         "current-track": "",
-        "track-list": [],
+        trackList: [],
         render: function () {
+            // Render now playing text using transparencyjs
             Transparency.render(document.getElementById('now-playing'), this);
-            Transparency.render(document.getElementById('recent-tracks'), this);
+
+            // Add tracklist to html
+            this.trackList.forEach(function (item) {
+                document.querySelector("#track-list").innerHTML += '<li><div><img src="' + item.imgSrc + '"><span>' + item.track + '</span></div></li>'
+            })
         }
     }
 
@@ -39,33 +44,15 @@
 
             return new Promise(function (resolve, reject) {
                 var request = new XMLHttpRequest(),
-                    limit = 10,
-                    url = "//ws.audioscrobbler.com/2.0?method=user.getrecenttracks&user=" + self.user + "&api_key=" + self.key + "&format=" + self.format + "&limit=" + limit
+                    limit = 50,
+                    url = "https://ws.audioscrobbler.com/2.0?method=user.getrecenttracks&user=" + self.user + "&api_key=" + self.key + "&format=" + self.format + "&limit=" + limit
 
                 request.open('GET', url, true)
 
                 request.onload = function() {
                     if (request.status >= 200 && request.status < 400) {
-
-                        var data = JSON.parse(request.responseText),
-                            tracks = data.recenttracks.track,
-                            artist = data.recenttracks.track[0].artist["#text"],
-                            song = data.recenttracks.track[0].name
-
-                        content["current-track"] = artist + " - " + song
-
-                        tracks.forEach(function (track) {
-                            var artist = track.artist["#text"],
-                                song = track.name,
-                                trackName = artist + " - " + song
-
-                            content["track-list"].push({
-                                track: trackName
-                            })
-                        })
-
+                        var data = JSON.parse(request.responseText);
                         resolve(data)
-
                     } else {
                         reject('error')
                     }
@@ -80,7 +67,38 @@
         },
         getData: function () {
             this.getRecentTracks()
-                .then(function () {
+                .then(function (data) {
+                    var tracks = data.recenttracks.track,
+                        tracksWithIMG = [],
+                        tracksWithMBID = []
+
+                    content["current-track"] = data.recenttracks.track[0].artist["#text"] + " - " + data.recenttracks.track[0].name
+
+                    // Filter array by images that have an image
+                    function filterByIMG(item) {
+                        if (item.image[3]["#text"] !== "") {
+                            return item
+                        }
+                    }
+
+                    // Filter array by images that have an mbid
+                    function filterByMBID(item) {
+                        if (item.mbid !== "") {
+                            return item
+                        }
+                    }
+
+                    tracksWithIMG = tracks.filter(filterByIMG)
+                    tracksWithMBID = tracks.filter(filterByMBID)
+
+                    // Only push 10 items to the content object
+                    for(var i = 0; i < 12; i++) {
+                        content.trackList.push({
+                            track: tracksWithIMG[i].artist["#text"] + " - " + tracksWithIMG[i].name,
+                            imgSrc: tracksWithIMG[i].image[3]["#text"]
+                        })
+                    }
+
                     content.render()
                 })
                 .catch(function (err) {
